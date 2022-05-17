352a3c6bf7bdbe7ba276b4cc22d20028bd9e1b98
Gareth Dwyer <gdwyer59@hotmail.com>
2022-05-17 11:06:22 +0100

    Official Android 12 Splash Screen implemented

--- app/src/main/java/com/garethdwyer/germanquiz/MainActivity.kt
+++ app/src/main/java/com/garethdwyer/germanquiz/MainActivity.kt
@@ -2,13 +2,21 @@ package com.garethdwyer.germanquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.garethdwyer.germanquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
