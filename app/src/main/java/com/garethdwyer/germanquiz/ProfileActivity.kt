4b488f3e80dc021af7a98fe3d3a1202863bf4ad6
Gareth Dwyer <gdwyer59@hotmail.com>
2022-05-17 23:44:53 +0100

    Logging in & out functionality working

--- app/src/main/java/com/garethdwyer/germanquiz/ProfileActivity.kt
+++ app/src/main/java/com/garethdwyer/germanquiz/ProfileActivity.kt
@@ -1,11 +1,32 @@
 package com.garethdwyer.germanquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.garethdwyer.germanquiz.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signOut.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this@ProfileActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        /*
+        val user = FirebaseAuth.getInstance().currentUser
+        val reference = FirebaseDatabase.getInstance().getReference("Users")
+        val userID = user!!.uid
+        val name = reference.child(userID).child("fullName")
+
+        binding.textView.setText(name)
+        */
+
     }
 }
