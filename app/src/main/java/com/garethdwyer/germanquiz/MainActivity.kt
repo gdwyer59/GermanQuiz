293274a50c46bff82cc7b2086a6115266a8c6d42
Gareth Dwyer <gdwyer59@hotmail.com>
2022-05-17 13:55:43 +0100

    launching RegisterUser Activity via intent not working

--- app/src/main/java/com/garethdwyer/germanquiz/MainActivity.kt
+++ app/src/main/java/com/garethdwyer/germanquiz/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.garethdwyer.germanquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.garethdwyer.germanquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    //private lateinit var register: TextView
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen().apply {
@@ -16,8 +19,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //register = binding.register
+        binding.register.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegisterUser::class.java)
+            startActivity(intent)
+        }
     }
 }
